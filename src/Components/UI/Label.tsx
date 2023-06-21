c7d0dfc164211778a43d568bd5c2e34de6208dc3
Renato Pozzi <renato.pozzi.dev@gmail.com>
2023-06-21 23:12:38 +0200

    feat: add label component

---
+++ src/Components/UI/Label.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export interface LabelProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {}
+
+const Label = React.forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <label
+        className={cn(
+          "block text-sm text-zinc-800 font-medium dark:text-white",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Label.displayName = "Label";
+
+export { Label };
