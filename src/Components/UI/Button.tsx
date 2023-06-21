8e8098a4a7864cbdbb0ab2cbe363db9262b87307
Renato Pozzi <renato.pozzi.dev@gmail.com>
2023-06-21 23:10:29 +0200

    feat: add button component

---
+++ src/Components/UI/Button.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
+
+export const createButtonVariants = cva(
+  "inline-flex text-sm font-semibold shadow-sm border transition-colors focus:outline-none rounded focus:ring",
+  {
+    variants: {
+      variant: {
+        primary:
+          "border-zinc-900 bg-zinc-800 text-white hover:bg-zinc-900 focus:ring-zinc-400 disabled:bg-zinc-600",
+        secondary:
+          "border-zinc-150 bg-white text-zinc-800 hover:bg-zinc-50 hover:text-zinc-900 focus:ring-zinc-100",
+      },
+      size: {
+        lg: "py-3 px-6",
+        base: "py-2 px-4",
+        sm: "py-1 px-3",
+      },
+    },
+    defaultVariants: {
+      variant: "primary",
+      size: "base",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof createButtonVariants> {}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, ...props }, ref) => {
+    return (
+      <button
+        className={cn(createButtonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button };
