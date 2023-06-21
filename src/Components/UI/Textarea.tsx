cd2540731bf751b540fae2650a45c6bfc6078e70
Renato Pozzi <renato.pozzi.dev@gmail.com>
2023-06-21 23:14:03 +0200

    feat: add textarea component

---
+++ src/Components/UI/Textarea.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+
+export interface TextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <textarea
+        className={cn(
+          "py-3 px-4 block w-full border-gray-200 rounded-sm text-sm focus:border-blue-500 focus:ring-blue-500 dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+
+Textarea.displayName = "Textarea";
+
+export { Textarea };
