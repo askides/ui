6c4ec61aeb3c9da2eba91d14585dc1fbb73237a6
Renato Pozzi <renato.pozzi.dev@gmail.com>
2023-07-02 14:59:47 +0200

    fix: improve code

--- src/_comp/docs/Markdown.tsx
+++ src/_comp/docs/Markdown.tsx
@@ -1,11 +1,5 @@
 import { Preview } from "@/_comp/docs/Preview";
-import { Button } from "@/_comp/ui/Button";
 import { useMDXComponent } from "next-contentlayer/hooks";
-
-export const MDXComponents = {
-  Button,
-  Preview,
-};
 
 export function Markdown({ mdx }: { mdx: string }) {
   const Comp = useMDXComponent(mdx);
@@ -13,7 +7,7 @@ export function Markdown({ mdx }: { mdx: string }) {
   return (
     <article className="prose prose-h2:mb-2 prose-p:font-light max-w-2xl">
       {/* @ts-ignore */}
-      <Comp components={MDXComponents} />
+      <Comp components={{ Preview }} />
     </article>
   );
 }
